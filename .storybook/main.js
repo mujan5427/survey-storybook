bfa1e3f22fbdc2efbaf072ea396cfec9eeb1fd1a
Justin Ho <mujan5427@gmail.com>
2020-03-07 21:09:17 +0800

    調整 storybook-readme config 設定

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,24 @@
 module.exports = {
-    stories: ['../storybook/**/*.stories.[tj]s', '../storybook/**/*.stories.mdx'],
-    addons: ['@storybook/addon-docs',
-             '@storybook/addon-knobs/register'
-            ],
+    stories: ['../storybook/**/*.stories.[tj]s',],
+    addons: [
+      'storybook-readme/register',
+      '@storybook/addon-knobs/register',
+    ],
+    webpackFinal: async (config, { configType }) => {
+        // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+        // You can change the configuration based on that.
+        // 'PRODUCTION' is used when building the static version of storybook.
+
+        config.module.rules.push({
+          test: /\.md$/,
+          use: [
+            {
+              loader: 'markdown-loader',
+            }
+          ]
+        });
+    
+        // Return the altered config
+        return config;
+      },
 };
---
+++ .storybook/preview.js
@@ -0,0 +1,10 @@
+import { addParameters } from '@storybook/react';
+
+addParameters({
+  theme: {
+    base: 'light',
+    brandTitle: 'Spark Core',
+    brandUrl: 'https://spark-core.com',
+    brandImage: 'https://t.kfs.io/upload_images/67817/Spark_large.png'
+  }
+});
